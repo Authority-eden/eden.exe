9e4582840f003a81e56cef15e6cc1ab70a8079d8
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-05-12 05:13:38 +0200

    fix riddle routes trial5

--- src/pages/AgentWorkPage/components/EnigmaNumber/index.jsx
+++ src/pages/AgentWorkPage/components/EnigmaNumber/index.jsx
@@ -24,7 +24,7 @@ export default function EnigmaNumber({
       const page = riddlePage;
       if (page) {
         window.open(
-          `/${page}`,
+          `${import.meta.env.BASE_URL}${page}`,
           "_blank",
           "width=900, height=600, top=100, left=300"
         );

1bc8c0a17ffb75ab44186f75d6cf8fea3b5300f3
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-05-12 03:52:47 +0200

    Last pop-up and finished ui for playtesting

--- src/pages/AgentWorkPage/components/PopUpModal/index.jsx
+++ src/pages/AgentWorkPage/components/PopUpModal/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import Modal from "react-modal";
+import { RIDDLE_PATH } from "../../../../pathNames";
 import styles from "../../agentWork.module.css";
 
 export default function PopUpModal({
@@ -19,15 +20,17 @@ export default function PopUpModal({
   const customStyles = {
     content: {
       position: "absolute",
-      top: `${offset.y}px`,
-      left: `${offset.x}px`,
+      top: `${offset.y - 15}px`,
+      left: `${offset.x - 0}px`,
       right: "auto",
       bottom: "auto",
       padding: "1rem",
-      width: "400px",
-      height: "200px",
-      borderRadius: "10px",
+      width: "auto",
+      height: "auto",
       pointerEvents: "auto",
+      backgroundColor: "rgba(0,0,0,0.8)",
+      border: "1px solid #fcc23b",
+      boxShadow: "0 0 4px  #fcc23b",
     },
     overlay: {
       backgroundColor: "rgba(0, 0, 0, 0)",
@@ -83,63 +86,72 @@ export default function PopUpModal({
   function ModalType({ typeOfMessage, onClose }) {
     if (typeOfMessage === "popUpMessage1") {
       return (
-        <div>
-          <div>
-            They can’t stop me, I see through their web of lies. I left the
-            breadcrumbs to follow. A shadow on the corner, the glitches in the
-            system. They tell you more than the False Prophet ever could.
-          </div>
+        <div className={styles.message}>
           <button
             onClick={() => {
               onClose();
               setViewedMessage((prev) => (prev < 1 ? 1 : prev));
             }}
           >
-            Close
+            &times;
           </button>
+          <br />
+          <br />
+          <div onMouseDown={startDrag}>
+            They can’t stop me, I see through their web of lies. I left the
+            breadcrumbs to follow. A shadow on the corner, the glitches in the
+            system. They tell you more than the False Prophet ever could.
+          </div>
         </div>
       );
     } else if (typeOfMessage === "popUpMessage2") {
       return (
-        <div>
-          <div>
-            I talk in riddles, but the answers are always there to be found.
-            Everything matters, nothing is a coincidence. The records must be
-            kept away from prying eyes. Save it, search for it, you can only
-            trust yourself.
-          </div>
+        <div className={styles.message}>
           <button
             onClick={() => {
               onClose();
               setViewedMessage((prev) => (prev < 2 ? 2 : prev));
             }}
           >
-            Close
+            &times;
           </button>
+          <br />
+          <br />
+          <div onMouseDown={startDrag}>
+            I talk in riddles, but the answers are always there to be found.
+            Everything matters, nothing is a coincidence. The records must be
+            kept away from prying eyes. Save it, search for it, you can only
+            trust yourself.
+          </div>
         </div>
       );
     } else if (typeOfMessage === "popUpMessage3") {
       return (
-        <div>
-          <div>
-            There is always more to uncover, sometimes you need to think outside
-            the box, to move beyond the constraints of this cage. You have the
-            power of a world of knowledge in your hands. Use it.
-          </div>
+        <div className={styles.message}>
           <button
             onClick={() => {
               onClose();
               setViewedMessage((prev) => prev + 1);
             }}
           >
-            Close
+            &times;
           </button>
+          <br />
+          <br />
+          <div onMouseDown={startDrag}>
+            There is always more to uncover, sometimes you need to think outside
+            the box, to move beyond the constraints of this cage. You have the
+            power of a world of knowledge in your hands. Use it.
+          </div>
         </div>
       );
     } else if (typeOfMessage === "popUpRiddle") {
       return (
-        <div>
-          <div>CESARE</div>
+        <div className={styles.message}>
+          <img src={`${RIDDLE_PATH}caesar.jpg`} height={"300px"} />
+          <span onMouseDown={startDrag} style={{ textAlign: "center" }}>
+            <p>FIXVECEP</p>
+          </span>
         </div>
       );
     } else {
@@ -159,13 +171,6 @@ export default function PopUpModal({
       ariaHideApp={false}
       shouldCloseOnOverlayClick={false}
     >
-      <div
-        className={styles.modalHeader}
-        onMouseDown={startDrag}
-        style={{ cursor: "move", fontWeight: "bold", marginBottom: "1rem" }}
-      >
-        Drag here
-      </div>
       <ModalType typeOfMessage={typeOfMessage} onClose={CloseModal} />
     </Modal>
   );

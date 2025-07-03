081065fb0f3a30e42799ca6a2220571b44705e8d
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-07-03 19:49:40 +0200

    test 13

--- src/main.jsx
+++ src/main.jsx
@@ -19,12 +19,28 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         {/* <Route path="/app" element={<App />} /> */}
-        <Route path=" " element={<App />} />
-        <Route path="cut_throat" element={<CutThroat />} />
-        <Route path="color_coded" element={<ColorCoded />} />
-        <Route path="half_half" element={<HalfHalf />} />
-        <Route path="agent_work" element={<AgentWork />} />
-        <Route path="revolution" element={<Revolution />} />
+
+        <Route path={`${import.meta.env.BASE_URL}`} element={<App />} />
+        <Route
+          path={`${import.meta.env.BASE_URL}cut_throat`}
+          element={<CutThroat />}
+        />
+        <Route
+          path={`${import.meta.env.BASE_URL}color_coded`}
+          element={<ColorCoded />}
+        />
+        <Route
+          path={`${import.meta.env.BASE_URL}half_half`}
+          element={<HalfHalf />}
+        />
+        <Route
+          path={`${import.meta.env.BASE_URL}agent_work`}
+          element={<AgentWork />}
+        />
+        <Route
+          path={`${import.meta.env.BASE_URL}revolution`}
+          element={<Revolution />}
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>

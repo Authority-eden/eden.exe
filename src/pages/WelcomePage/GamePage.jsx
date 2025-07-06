c6bdcd8a1a6c2e0643563da6d1574909549a2ff2
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-07-06 23:09:09 +0200

    fix2

--- src/pages/WelcomePage/GamePage.jsx
+++ src/pages/WelcomePage/GamePage.jsx
@@ -282,8 +282,9 @@ export default function GamePage() {
             </li>
           </ul>
         </div>
+        <br />
       </div>
-      <br />
+
       <hr />
 
       <div className={styles.uniCredits}>

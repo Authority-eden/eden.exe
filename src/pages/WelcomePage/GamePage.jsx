fe4efb61c45de31ef0c28a3be0b46d6faa82536d
Miriam Bissoli <126024992+miriambixso02@users.noreply.github.com>
2025-07-06 22:25:18 +0200

    resources

--- src/pages/WelcomePage/GamePage.jsx
+++ src/pages/WelcomePage/GamePage.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import styles from "./gamePage.module.css";
 import { IDENTITY_PATH } from "../../pathNames";
 import Slider from "./components/index.jsx";
 
 export default function GamePage() {
+  const [isDSVisible, setIsDSVisible] = useState(false);
+  const [isPvsSVisible, setIsPvsSVisible] = useState(false);
+
   return (
     <div className={styles.container}>
       <img
@@ -26,12 +30,12 @@ export default function GamePage() {
       <div className={styles.paragraph}>
         <p className={styles.text}>
           The year 2046. You are an Agent of The Authority, the world order that{" "}
-          <span className={styles.highlight}>keeps</span> you, your family and
-          all of <span className={styles.highlight}>your data safe.</span> You
-          do not know the exact details of how this came to be, you were too
-          young to remember. But you know of it, the Collapse, the rise of The
-          Authority, and the better world it created for everyone, more
-          peaceful, safe, and free.
+          keeps you, your family and all of{" "}
+          <span className={styles.highlight}>your data safe.</span> You do not
+          know the exact details of how this came to be, you were too young to
+          remember. But you know of it, the Collapse, the rise of The Authority,
+          and the better world it created for everyone, more peaceful, safe, and
+          free.
           <br />
           <br />
           You are relatively new to your job but you quickly made friends with
@@ -60,10 +64,12 @@ export default function GamePage() {
 
       <div className={styles.credentials}>
         <h1>Begin your work as an agent</h1>
+
+        <br />
+        <br />
         <br />
         <br />
 
-        <br />
         <button
           onClick={() =>
             window.open(`${import.meta.env.BASE_URL}app`, "_blank")
@@ -71,13 +77,15 @@ export default function GamePage() {
         >
           Play
         </button>
+
         <br />
         <br />
         <br />
         <br />
         <br />
+        <br />
+
         <hr />
-        <br />
       </div>
 
       <h1>The Project</h1>
@@ -120,140 +128,178 @@ export default function GamePage() {
           </span>{" "}
           Sometimes, to uncover the future, you must reassemble the past.
         </p>
-        <div className={styles.paragraph}>
-          <Slider></Slider>
-        </div>
-
         <br />
+        <Slider></Slider>
+      </div>
 
-        <hr />
+      <hr />
+      <br />
+      <h1>Resources</h1>
+      <div className={styles.research}>
+        <button
+          onClick={() => {
+            setIsDSVisible(!isDSVisible);
+          }}
+        >
+          Double Standards
+        </button>
+        <button
+          onClick={() => {
+            setIsPvsSVisible(!isPvsSVisible);
+          }}
+        >
+          Privacy VS Surveillance
+        </button>
+      </div>
+      <div
+        style={{
+          display: isDSVisible ? "block" : "none",
+        }}
+      >
         <br />
-
         <h3>General Double Standard Papers</h3>
+        <div></div>
+        <ul>
+          <li>
+            <a
+              href="https://link.springer.com/article/10.1007/s10551-010-0654-3"
+              target="_blank"
+            >
+              Double Standards: The Role of Techniques of Neutralization
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://journals.sagepub.com/doi/epub/10.1177/1474885114546137"
+              target="_blank"
+            >
+              Autonomy and cultural practices: The risk of double standards
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://site.unibo.it/canadausa/it/articoli/double-standard-cosa-e-e-perche-combatterlo"
+              target="_blank"
+            >
+              Double Standard: cosa è e perché combatterlo
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://doaj.org/article/20ececaaab444a0dbde0603f31460dc0"
+              target="_blank"
+            >
+              The Concept of “Double Standards” in the Sociolinguistics
+              Discourse
+            </a>
+          </li>
+          <li>
+            <a href="https://www.jstor.org/stable/223435" target="_blank">
+              Double Standards for Competence: Theory and Research
+            </a>
+          </li>{" "}
+          <li>
+            <a
+              href="https://academic.oup.com/bjaesthetics/article/64/1/67/7180751?"
+              target="_blank"
+            >
+              Double-Standard Moralism: Why We Can Be More Permissive Within Our
+              Imagination
+            </a>
+          </li>
+        </ul>
+      </div>
+      <br />
+      <div
+        style={{
+          display: isPvsSVisible ? "block" : "none",
+        }}
+      >
+        <h3>Privacy VS Surveillance Double Standard Papers</h3>
         <div>
           <ul>
             <li>
               <a
-                href="https://journals.rudn.ru/semiotics-semantics/article/view/7211/6664"
+                href="https://onlinelibrary.wiley.com/doi/epdf/10.1111/socf.12040"
                 target="_blank"
               >
-                Paper
+                Sociological Reflections on Security Through Surveillance
               </a>
             </li>
             <li>
               <a
-                href="https://site.unibo.it/canadausa/it/articoli/double-standard-cosa-e-e-perche-combatterlo"
+                href="https://www.sciencedirect.com/science/article/abs/pii/S0963868707000492?"
                 target="_blank"
               >
-                Paper
+                Internet privacy concerns and beliefs about government
+                surveillance – An empirical investigation
               </a>
             </li>
             <li>
               <a
-                href="https://link.springer.com/article/10.1007/s10551-010-0654-3"
+                href="https://www.tandfonline.com/doi/full/10.1080/2573234X.2021.1920856"
                 target="_blank"
               >
-                Paper
-              </a>
-            </li>
-            <li>
-              <a href="https://www.jstor.org/stable/223435" target="_blank">
-                Paper
+                Balancing privacy rights and surveillance analytics: a decision
+                process guide
               </a>
             </li>
             <li>
               <a
-                href="https://journals.rudn.ru/semiotics-semantics/article/view/7211/6664"
+                href="https://cris.unibo.it/retrieve/8e1cbd36-6950-4acd-9e9f-1ada9c9f49a2/sorveglianza%20federalismi%20Mollo.pdf"
                 target="_blank"
               >
-                Paper
+                Sorveglianza di massa, rispetto della vita privata e trattamento
+                di categorie particolari di dati nel quadro multilivello di
+                tutela della persona
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.sciencedirect.com/science/article/abs/pii/S0740624X15000246"
+                target="_blank"
+              >
+                Public opinion on National Security Agency surveillance
+                programs: A multi-method approach
               </a>
             </li>{" "}
             <li>
               <a
-                href="https://academic.oup.com/bjaesthetics/article/64/1/67/7180751?utm_source=chatgpt.com&login=true#436466792"
+                href="https://www.aei.org/wp-content/uploads/2015/04/Cyber-surveillance-regulations.pdf?x85095"
                 target="_blank"
               >
-                Paper
+                Cyber surveillance regulations: Is the United States asking
+                China to accept a double standard?
               </a>
             </li>
           </ul>
-
-          <h3>Privacy VS Surveillance Double Standard Papers</h3>
-          <div>
-            <ul>
-              <li>
-                <a
-                  href="https://journals.rudn.ru/semiotics-semantics/article/view/7211/6664"
-                  target="_blank"
-                >
-                  Paper
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://site.unibo.it/canadausa/it/articoli/double-standard-cosa-e-e-perche-combatterlo"
-                  target="_blank"
-                >
-                  Paper
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://link.springer.com/article/10.1007/s10551-010-0654-3"
-                  target="_blank"
-                >
-                  Paper
-                </a>
-              </li>
-              <li>
-                <a href="https://www.jstor.org/stable/223435" target="_blank">
-                  Paper
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://journals.rudn.ru/semiotics-semantics/article/view/7211/6664"
-                  target="_blank"
-                >
-                  Paper
-                </a>
-              </li>{" "}
-              <li>
-                <a
-                  href="https://academic.oup.com/bjaesthetics/article/64/1/67/7180751?utm_source=chatgpt.com&login=true#436466792"
-                  target="_blank"
-                >
-                  Paper
-                </a>
-              </li>
-            </ul>
-          </div>
-          <br />
-          <hr />
-          <br />
-
-          <h2>Credits</h2>
-          <ul>
-            <li>Bertagnin Bianca</li>
-            <li>Bissoli Miriam</li>
-            <li>Casini Francesca</li>
-            <li>Figueiredo Caeiro Alice</li>
-            <li>Garipova Dariia</li>
-            <li>De Jesus Pereira Beatriz Inês</li>
-          </ul>
-          <div>
-            <p>
-              Polytechnic University of Milan
-              <br />
-              School of Design
-              <br />
-              Complex Artifacts and System Design Studio
-              <br />
-              AY 2024 — 2025
-            </p>
-          </div>
         </div>
+      </div>
+
+      <hr />
+
+      <div className={styles.uniCredits}>
+        <h2>Credits</h2>
+        <ul>
+          <li>Bertagnin Bianca</li>
+          <li>Bissoli Miriam</li>
+          <li>Casini Francesca</li>
+          <li>Figueiredo Caeiro Alice</li>
+          <li>Garipova Dariia</li>
+          <li>De Jesus Pereira Beatriz Inês</li>
+        </ul>
+      </div>
+      <div className={styles.uniCredits}>
+        <p>
+          Polytechnic University of Milan
+          <br />
+          School of Design
+          <br />
+          Complex Artifacts and System Design Studio
+          <br />
+          AY 2024 — 2025
+        </p>
+        <br />
       </div>
     </div>
   );
